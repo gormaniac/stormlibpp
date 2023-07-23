7afa1bb112e67c220d18b89b10c34d0a123c15ee
gormaniac <83027042+gormaniac@users.noreply.github.com>
2023-07-23 14:02:56 -0600

    build: release v0.3.1

--- src/stormlibpp/__init__.py
+++ src/stormlibpp/__init__.py
@@ -29,7 +29,7 @@ are briefly described below:
 
 """
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 # TODO - Do we want to change this to only import submods, or do we even need that?
 from . import errors
