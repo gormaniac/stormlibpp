e317d173bc561cfebcab23a610f5e0a2a724d81f
gormaniac <83027042+gormaniac@users.noreply.github.com>
2023-08-10 10:42:10 -0600

    feat(stormgen): expand on initial stormgen framework

--- src/stormlibpp/stormgen/__init__.py
+++ src/stormlibpp/stormgen/__init__.py
@@ -10,51 +10,20 @@ import marko
 from .. import StormNode
 
 
-class Part:
-    """An element is all blocks between any two headings."""
+class DocPart:
+    """A DocPart is all blocks between any two headings, including the 1st heading."""
 
-    def __init__(self, heading: marko.block.Heading) -> None:
-        self.heading = heading
+    def __init__(self, heading: marko.block.Heading | None) -> None:
         self.blocks: list[marko.block.BlockElement] = []
-        self.children = []
+        self.heading = heading
+        self.level = 0
+
+        if self.heading is not None:
+            self.level = self.heading.level
 
     def add_block(self, block: marko.block.BlockElement):
         self.blocks.append(block)
 
-
-class Section:
-    """All Markdown tokens between two headings of the same level.
-    
-    ``Section``s are organized into a list of ``Part``s. Each part
-    is all content between two headings including the first heading.
-
-    A "root section" is a ``Section`` that exists because there is no top level
-    heading in the original Markdown document. This section cannot have a level
-    because there is no heading, so ``level`` will equal 0.
-
-    A root section is created when no ``heading`` is passed to the constructor.
-    """
-
-    def __init__(self, heading: marko.block.Heading | None = None) -> None:
-        self.heading = heading
-        self.cur = Part(self.heading)
-        self.level = 0
-        self.parts: list[Part] = [self.cur]
-
-        self.root = True if heading is None else False
-        """The root section holds all blocks that exists before any headings."""
-
-        if not self.root:
-            self.level = heading.level
-
-    def add_part(self, heading: marko.block.Heading):
-        # The first part will always be in the list.
-        if self.cur not in self.parts:
-            self.parts.append(self.cur)
-        self.cur = Part(heading)
-
-    def add_block(self, block: marko.block.BlockElement):
-        self.cur.add_block(block)
 
 class StormGenDoc:
     """A Markdown document that is ready for processing with stormgen.
@@ -67,21 +36,100 @@ class StormGenDoc:
     """
 
     def __init__(self, text: str) -> None:
-        self.ast = marko.parse(text)
-        self.parts = []
+        self.ast: list[marko.block.BlockElement] = marko.parse(text).children
+        """All blocks in the input text, in top-to-bottom order of appearance."""
+        self.parts: list[DocPart] = []
+        """A list of all ``DocPart``s in the input text, in order of appearance.
+        
+        A ``DocPart`` is all blocks between two headers.
+        """
 
-    def parse(self) -> list[Section]:
-        part = Part(heading=None)
-        for child in self.ast.children:
+    def parse(self) -> None:
+        """Parse the input text into a list of blocks and a list of ``DocPart``s."""
+
+        part = DocPart(heading=None)
+
+        for child in self.ast:
             if isinstance(child, marko.block.Heading):
                 self.parts.append(part)
-                part = Part(heading=child)
+                part = DocPart(heading=child)
+
             part.add_block(child)
+
         return
 
 
-class StormGenElement:
-    """Processable pieces of a StormGenDoc."""
+class DocNode:
+    """A node defined in Markdown - as a grouping of ``DocPart``s."""
+
+    def __init__(self, parts: list[DocPart], form: str) -> None:
+        self.parts = parts
+        self.form = form
+        self.valu: str = ""
+        self.props: list[DocPart] = []
+        self.tags: list[DocPart] = []
+        self.edges: list[DocPart] = []
+
+        self.extract()
+
+    def extract(self):
+        """Extract all parts of a DocNode from the input parts."""
+        pass
+
+
+class DocExtractor:
+    """Extracts ``DocNode``s from a ``StormGenDoc``."""
+
+    types = (
+        "embedded",
+        "forms",
+        "full",
+    )
+
+    def __init__(
+        self,
+        doc: StormGenDoc,
+        /,
+        doc_type: str = "full",
+        form: str = "",
+        nodes_header: str = "Nodes",
+    ) -> None:
+        self.doc = doc
+        self.dtype = doc_type
+        self.form = form
+        self.nheader = nodes_header
+
+    def _extract_embedded(self):
+        pass
+
+    def _extract_forms(self):
+        pass
+
+    def _extract_full(self):
+        pass
+
+    def extract(self) -> list[DocNode]:
+        """Get the ``DocNode``s from the input document."""
+
+        match self.dtype:
+            case "embedded":
+                return self._extract_embedded()
+            case "forms":
+                return self._extract_forms()
+            case "full":
+                return self._extract_full()
+
+
+class NodesConverter:
+    """Convert ``DocNode``s to ``StormNode``s."""
+
+    def __init__(self, doc_nodes: list[DocNode], comments: bool = False,) -> None:
+        self.save_comments = comments
+        self.doc_nodes = doc_nodes
+        self.storm_nodes: list[StormNode] = []
+    
+    def convert(self) -> None:
+        pass
 
 
 def parse(text: str) -> StormGenDoc:
@@ -90,9 +138,29 @@ def parse(text: str) -> StormGenDoc:
     return doc
 
 
-def extract(doc: StormGenDoc) -> list[StormGenElement]:
-    pass
+def extract(
+    doc: StormGenDoc,
+    doc_type: str = "full",
+    nodes_header: str = "Nodes",
+    form: str = "",
+) -> list[DocNode]:
+    return DocExtractor(
+        doc,
+        doc_type=doc_type,
+        nodes_header=nodes_header,
+        form=form,
+    ).extract()
 
 
-def convert(doc: StormGenDoc) -> list[StormNode]:
-    pass
+def convert(
+    doc: StormGenDoc,
+    comments: bool = False,
+    doc_type: str = "full",
+    nodes_header: str = "Nodes",
+    form: str = "",
+) -> list[StormNode]:
+    cvertr = NodesConverter(
+        extract(doc, doc_type, nodes_header, form), comments=comments
+    )
+    cvertr.convert()
+    return cvertr.storm_nodes
