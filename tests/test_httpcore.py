d6d43329c10a9865637a5f047bf579f0f4e36228
gormaniac <83027042+gormaniac@users.noreply.github.com>
2023-08-13 16:32:20 -0400

    test: add a callStorm test

--- tests/test_httpcore.py
+++ tests/test_httpcore.py
@@ -1,6 +1,3 @@
-import os
-import sys
-
 import synapse.tests.utils as s_test
 
 import stormlibpp
@@ -14,7 +11,17 @@ class TestHttpCore(s_test.SynTest):
             async with stormlibpp.HttpCortex(
                 "https://127.0.0.1:4443", ssl_verify=False, usr="test", pwd="test"
             ) as hcore:
-                return
+                assert hcore.sess.cookie_jar._cookies != {}
+
+    async def test_callStorm(self):
+        async with self.getTestCore(conf={"https:port": 4443}) as core:
+            await core.addUser("test", passwd="test")
+
+            async with stormlibpp.HttpCortex(
+                "https://127.0.0.1:4443", ssl_verify=False, usr="test", pwd="test"
+            ) as hcore:
+                retn = await hcore.callStorm("return('test')")
+                assert retn["result"] == "test"
 
     async def test_storm(self):
         async with self.getTestCore(conf={"https:port": 4443}) as core:
@@ -26,5 +33,4 @@ class TestHttpCore(s_test.SynTest):
                 msgs = []
                 async for msg in hcore.storm("help"):
                     msgs.append(msg)
-                print(msgs)
                 self.stormHasNoWarnErr(msgs)
