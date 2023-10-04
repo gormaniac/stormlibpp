68428dfed8cb2a86e457e09f01d1aa39d3760867
gormaniac <83027042+gormaniac@users.noreply.github.com>
2023-10-03 21:19:28 -0600

    feat: support env vars for user/pass in import

--- src/stormlibpp/import.py
+++ src/stormlibpp/import.py
@@ -1,8 +1,4 @@
-"""Recursively execute all Storm scripts in a given folder on a given Synapse Cortex over HTTP.
-
-If using the --http argument, a password will be prompted for. The CORTEX_PASS environment
-variable may be used to avoid the prompt.
-"""
+"""Recursively execute all Storm scripts in a given folder on a given Synapse Cortex over HTTP."""
 
 
 import argparse
@@ -23,6 +19,7 @@ import synapse.common as s_common
 import synapse.telepath as s_telepath
 import yaml
 
+from ._args import USER_PARSER
 from .httpcore import HttpCortex
 from .output import handle_msg, log_storm_msg, OUTP
 from .stormcli import start_storm_cli
@@ -176,7 +173,7 @@ def cortex_proxy_contextmanager(cortex_url):
 
 
 def get_args(argv: list[str]):
-    parser = argparse.ArgumentParser(description=__doc__)
+    parser = argparse.ArgumentParser(description=__doc__, parents=[USER_PARSER])
     parser.add_argument(
         "folders", help="The folder(s) containing Storm scripts to execute", nargs="+"
     )
@@ -220,18 +217,6 @@ def get_args(argv: list[str]):
         "--no-verify",
         help="Skips verification of the Cortex's certificate when using --http",
         action="store_true",
-    )
-    parser.add_argument(
-        "--user",
-        # FIXME - This explanation is backwards - --user overrides CORTEX_USER
-        help=(
-            "The Cortex user to authenticate with - by default the return of"
-            " getpass.getuser(). May override this with the CORTEX_USER env var."
-            " If neither --user or CORTEX_USER are passed, a prompt will appear"
-            " to either input a value or accept the default. Only works with --http."
-        ),
-        # FIXME - I don't think we actually want this default
-        default=getpass.getuser(),
     )
     parser.add_argument(
         "--view",
@@ -283,9 +268,9 @@ async def main(argv: list[str]):
         return "Can't use both --cortex and --local!"
     elif args.cortex:
         if args.http:
-            user, passwd = get_cortex_creds(args.user)
+            username, password = get_cortex_creds(args.user)
             core_obj = functools.partial(
-                HttpCortex, args.cortex, user, passwd, ssl_verify=not args.no_verify
+                HttpCortex, args.cortex, username, password, ssl_verify=not args.no_verify
             )
         else:
             core_obj = functools.partial(cortex_proxy_contextmanager, args.cortex)
