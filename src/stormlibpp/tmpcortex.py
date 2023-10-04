7958bd19a4b6480183010f038cf44f4bf6aac360
gormaniac <83027042+gormaniac@users.noreply.github.com>
2023-10-04 07:04:08 -0600

    feat: display help in tmpcortex main

--- src/stormlibpp/tmpcortex.py
+++ src/stormlibpp/tmpcortex.py
@@ -3,15 +3,32 @@
 
 import asyncio
 import contextlib
+import sys
+from typing import Iterable
 
 import synapse.common as s_common
 import synapse.cortex as s_cortex
 
 
+MAIN_HELP = """
+Start a temporary local Cortex.
+The HTTP API is enabled on port 4443.
+A user is created with the creds test:test.
+"""
+
+
 @contextlib.asynccontextmanager
-async def getTempCortexWithArgs(mods=None, conf=None, user="test", password="test"):
+async def getTempCortexWithArgs(
+    mods: Iterable | None = None,
+    conf: dict | None = None,
+    user: str = "test",
+    password: str = "test",
+    print_dir: bool = True,
+):
     with s_common.getTempDir() as dirn:
         async with await s_cortex.Cortex.anit(dirn, conf=conf) as core:
+            if print_dir:
+                print(f"Started a temporary Cortex from {dirn}")
             if mods:
                 for mod in mods:
                     await core.loadCoreModule(mod)
@@ -23,6 +40,10 @@ async def getTempCortexWithArgs(mods=None, conf=None, user="test", password="tes
 
 
 async def main():
+    if "-h" in sys.argv or "--help" in sys.argv:
+        print(MAIN_HELP)
+        sys.exit()
+
     async with getTempCortexWithArgs(conf={"https:port": 4443}) as proxy:
         try:
             while True:
