ddda1acd2449ffceda8c8821f17693506fbf3047
gormaniac <83027042+gormaniac@users.noreply.github.com>
2026-07-09 17:58:21 -0600

    build: include build storm packages in actions

--- scripts/pkgs/build.sh
+++ scripts/pkgs/build.sh
@@ -1,8 +1,10 @@
 # Build Storm Packages
 
+mkdir -p dist
+
 for pkg in $(ls src/pkgs/)
 do
     echo "Building the $pkg Storm Package"
     fileBase="src/pkgs/$pkg/$pkg"
-    pipenv run python3 -m synapse.tools.genpkg --save "$fileBase.json" "$fileBase.yaml"
+    pipenv run python3 -m synapse.tools.genpkg --save "dist/$pkg.json" "$fileBase.yaml"
 done
--- scripts/pkgs/push.sh
+++ scripts/pkgs/push.sh
@@ -1,4 +1,8 @@
 # Push Storm Packages to a Cortex
+#
+# This builds the Storm packages currently in source and pushes them to the specified Cortex.
+#
+# For production deployment, it's better to use the json files in the repo's releases.
 
 CORTEX=$1
 
@@ -7,8 +11,11 @@ then
     exit "Must specify the CORTEX env var!"
 fi
 
+mkdir -p dist
+make build-storm
+
 for pkg in $(ls src/pkgs/)
 do
     echo "Pushing the $pkg Storm Package to $CORTEX"
-    pipenv run python3 -m synapse.tools.genpkg --push "$CORTEX" "src/pkgs/$pkg/$pkg.json"
+    pipenv run python3 -m synapse.tools.genpkg --push "$CORTEX" "dist/$pkg.json"
 done
